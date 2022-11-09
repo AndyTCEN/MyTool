68eb58222eda2c8cba5436ac40953ddbaad2c2a0
Tsai Andy <andytsai19880122@hotmail.com>
2022-11-09 10:20:24 +0800

    新增弱掃處理Path_Traversal

---
+++ MyTool/WeakHelper/WeakHelper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WeakHelper
+{
+    public class WeakHelper
+    {
+        public static string Path_Traversal(string errstr)
+        {
+            return errstr.Replace("/", "").Replace(@"\", "").Replace("..", "").Replace("'", "");
+        }
+    }
+}
