1d793cfc274675c974eb03f0e5a035d41b598faa
xinglong20201101 <fe77122@gmail.com>
2023-06-08 18:08:01 +0800

    fix：修正Sln對Delegate參考

---
+++ MyTool/20230608 DelegatePic/20230608 DelegatePic/20230608 DelegatePic/Delegate/CountFunctionDelegate.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20230608_DelegatePic.Delegate
+{
+   public class CountFunctionDelegate
+    {
+
+        public delegate string Countfunction(params int[] par);
+        private readonly Dictionary<string, Countfunction> _countfunctionDic;
+        public CountFunctionDelegate()
+        {
+            _countfunctionDic = new Dictionary<string, Countfunction>
+            {
+                {"+",new Countfunction(Plus) },
+                { "-",new Countfunction(Mines)},
+            };
+        }
+
+        public string Counting(string FunName, params int[] par)
+        {
+            return _countfunctionDic[FunName].Invoke(par);
+        }
+
+        private string Plus(params int[] par)
+        {
+            int result = 0;
+            for (int i = 0; i < par.Length; i++)
+            {
+                result += par[i];
+            }
+            return result.ToString();
+        }
+
+        private string Mines(params int[] par)
+        {
+            int result = 0;
+            for (int i = 0; i < par.Length; i++)
+            {
+                result -= par[i];
+            }
+            return result.ToString();
+        }
+    }
+}
---
+++ MyTool/20230608 DelegatePic/20230608 DelegatePic/20230608 DelegatePic/Program.cs
@@ -0,0 +1,29 @@
+﻿using _20230608_DelegatePic.Delegate;
+using System;
+
+namespace _20230608_DelegatePic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("請輸入+或-");
+            string fun = Console.ReadLine();
+            Console.WriteLine("請輸入數字A");
+            string A = Console.ReadLine();
+            Console.WriteLine("請輸入數字B");
+            string B = Console.ReadLine();
+            Console.WriteLine("請輸入數字C");
+            string C = Console.ReadLine();
+
+            int intA = int.TryParse(A,  out int resultA)?resultA:0;
+            int intB = int.TryParse(B, out int resultB) ? resultB : 0;
+            int intC = int.TryParse(C, out int resultC) ? resultC : 0;
+
+            CountFunctionDelegate countFunctionDelegate = new CountFunctionDelegate();
+            var result = countFunctionDelegate.Counting(fun, intA, intB,intC);
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
